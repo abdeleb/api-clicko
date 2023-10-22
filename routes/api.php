dcf75a216b8fea68c7cd85c9e347cd01098e1ad0
abdeleb <abdeleb98@gmail.com>
2023-10-22 18:34:02 +0200

    Protected routes (only authenticated users can make requests)

--- routes/api.php
+++ routes/api.php
@@ -20,18 +20,20 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::group(array('prefix' => 'user'), function () {
+Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'user'], function () {
     Route::get('/', [ApiController::class, 'users']);
     Route::get('/top-domains', [ApiController::class, 'topDomains']);
     Route::post('/create', [ApiController::class, 'createUser']);
     Route::get('/{id}', [ApiController::class, 'showUser']);
     Route::post('/edit/{id}', [ApiController::class, 'updateUser']);
     Route::delete('/{id}', [ApiController::class, 'deleteUser']);
+
+    Route::get('/logout', [ApiAuthController::class, 'logout']);
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/logout', [ApiAuthController::class, 'logout']);
 });
 
 Route::post('/register', [ApiAuthController::class, 'register']);
 Route::post('/login', [ApiAuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/logout', [ApiAuthController::class, 'logout']);
-});
